0aa11cffe9e88a11ccf09c366195d8c51da4afb7
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2022-04-07 07:39:53 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from attr import attributes
 from texttable import Texttable
 
 class DrawTable:
@@ -7,7 +8,6 @@ class DrawTable:
         self.table = Texttable()
         if(not header):
             array.insert(0,["-" for i in range(len(array[0]))])
-            # self.table.header([1 for i in range(len(array[0]))])
         self.table.add_rows(array)
         print(self.table.draw())
 
@@ -15,8 +15,8 @@ class Dataset:
     def __init__(self,filename):
         self.filename = filename
         self.dataset_array = [["Data not available"],["Check file name"]]
+        self.targets = self.trim_target_attribute()
         self.table = DrawTable()
-        
     
     def open_csv(self):
         self.dataset_array = []
@@ -27,31 +27,50 @@ class Dataset:
                 self.dataset_array.append(row)
                 line_count += 1
     
-    def get_dataset_sample(self):
+    def transpose(self,l):
+        return [list(i) for i in zip(*l)]
+    
+    def get_sample(self):
         dataset_sample = []
         for index in range(1,len(self.dataset_array)):
             dataset_sample.append(self.dataset_array[index][0])
         return dataset_sample
 
-    def get_dataset_attribute(self):
+    def get_attribute(self):
         dataset_attribute = []
         for index in range(1,len(self.dataset_array)):
             dataset_attribute.append(self.dataset_array[index][1:-1])
         return dataset_attribute
     
-    def get_dataset_target_attribute(self):
-        dataset_target_attribute = []
+    def trim_attribute(self):
+        attributes = self.transpose(self.get_attribute())
+        res = []
+        for i in range(len(attributes)):
+            res.append(list(set(attributes[i])))
+        return res
+    
+    def get_target_attribute(self):
+        target_attribute = []
         for index in range(1,len(self.dataset_array)):
-            dataset_target_attribute.append(self.dataset_array[index][-1])
-        return dataset_target_attribute
+            target_attribute.append(self.dataset_array[index][-1])
+        return target_attribute
+
+    def trim_target_attribute(self):
+        return list(set(self.get_target_attribute()))
+    
+    def calculate_entropy():
+        pass
         
     def testing(self):
-        print("testing ground")
         self.open_csv()
-        print("CSV")
-        self.table.draw(self.dataset_array)
-        print("Attribute")
-        self.table.draw(self.get_dataset_attribute(),header=False)
+        print("testing ground")
+        # print("CSV")
+        # self.table.draw(self.dataset_array)
+        # print("Attributes")
+        # self.table.draw(self.get_attribute(),header=False)
+        # print("target")
+        print(self.trim_attribute())
+        # print(self.trim_target_attribute())
                 
 def main():
     #clear terminal
